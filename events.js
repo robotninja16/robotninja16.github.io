5461e29229b28cdd6e123fd66f3458e820f67f78
robotninja16 <47307468+robotninja16@users.noreply.github.com>
2020-07-08 10:53:59 -0600

    Delete events.js

--- events.js
+++
@@ -1,8 +0,0 @@
-function mouseClick(event, url, realEvent) {
-    if (realEvent != undefined) {
-        realEvent.cancelable = true;
-        realEvent.preventDefault();
-    }
-    if (event.button == 1 || event.button == 2)
-        window.open(url);
-}
